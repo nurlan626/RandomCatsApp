6c6a4145dc5837d5155952717152cc57f543a7c0
nurlan626 <67388158+nurlan626@users.noreply.github.com>
2021-11-01 15:07:05 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const button = document.querySelector(".btn");
+const image = document.querySelector(".img");
+const url = "https://aws.random.cat/meow";
+
+async function fetchHandler() {
+    try {
+        const response = await fetch(url)
+        const data = await response.json();
+        image.src = data.file;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+button.addEventListener('click', () => {
+    let isLoaded = image.complete; 
+    if(isLoaded){
+        fetchHandler();
+    }
+});
